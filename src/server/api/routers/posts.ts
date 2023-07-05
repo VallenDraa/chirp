72e768cf4d90c70cfd7f698272824f0092881ab1
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-07-05 08:17:26 +0700

    Styled the main page

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,45 @@
+import { clerkClient } from "@clerk/nextjs";
+import { type User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  const { id, username, profileImageUrl } = user;
+
+  return { id, username, profileImageUrl };
+};
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    // get user data from clerk api
+    const users = await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    });
+
+    const filteredUsers = users.map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = filteredUsers.find((user) => user.id === post.authorId);
+
+      if (!author || !author.username) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author Not Found !",
+        });
+      }
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
   }),
 });
